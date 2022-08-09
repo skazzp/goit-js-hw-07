4728271ae4654df45b2c213d7c841bf0fce339ee
skazzp <skazzp@gmail.com>
2022-08-10 01:04:23 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(picture => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${picture.original}">
+            <img
+            class="gallery__image"
+            src="${picture.preview}"
+            data-source="${picture.original}"
+            alt="${picture.description}"
+            />
+        </a>
+    </div>
+    `;
+  })
+  .join('');
+gallery.innerHTML = markup;
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  console.log(event);
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const imageLink = event.target.dataset.source;
+  const instance = basicLightbox.create(`<img src ="${imageLink}">`);
+  instance.show();
+}
